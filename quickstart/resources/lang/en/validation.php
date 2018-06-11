1b87b8360d21d160471c5e2756015113c2e7333a
nguyenhoangnam1297 <hoangnamit1297@gmail.com>
2018-06-11 17:02:14 +0700

    A

--- quickstart/resources/lang/en/validation.php
+++ quickstart/resources/lang/en/validation.php
@@ -25,9 +25,9 @@ return [
     'before_or_equal' => 'The :attribute must be a date before or equal to :date.',
     'between' => [
         'numeric' => 'The :attribute must be between :min and :max.',
-        'file'    => 'The :attribute must be between :min and :max kilobytes.',
-        'string'  => 'The :attribute must be between :min and :max characters.',
-        'array'   => 'The :attribute must have between :min and :max items.',
+        'file' => 'The :attribute must be between :min and :max kilobytes.',
+        'string' => 'The :attribute must be between :min and :max characters.',
+        'array' => 'The :attribute must have between :min and :max items.',
     ],
     'boolean' => 'The :attribute field must be true or false.',
     'confirmed' => 'The :attribute confirmation does not match.',
